0bdfee8bd4a6f4b620ac01a2ac508ac3c489b4bc
Michael <mikeystoddard@gmail.com>
2020-04-28 18:55:42 -0700

    added chalk

--- bamazon.sql
+++ bamazon.sql
@@ -1,14 +1,30 @@
 -- step 1 creating the database, and dropping one if it already exists --
-DROP DATABASE IF EXISTS bamazon.sql;
+DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 -- products table --
 CREATE TABLE products (
-    item_db INTEGER auto_increment NOT NULL PRIMARY KEY,
+    item_id INTEGER auto_increment NOT NULL PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
     price DECIMAL (10,3) NOT NULL,
-    stock_quantity INTEGER(10) NOT NULL,
-    PRIMARY KEY (item_id)
+    stock_quantity INT(10) NOT NULL
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+
+      ("Dog Toy", "Dog", 10, 100);
+      ("Cat Toy", "Cat", 10, 500);
+      ("Collars/Leashes", "Pet Accessories", 25, 100);
+      ("Dog Shoes", "Dog", 30, 30);
+      ("Cat Shoes", "Car", 30, 100);
+      ("Pet Food", "Food", 15, 1000);
+      ("Pet Treats", "Food", 5, 400);
+      ("Pet Tags", "Pet Accessories", 15, 300);
+      ("Kitty Litter", "Cats", 30, 230);
+      ("Pee Pad", "Dog", 50, 150);
+    -- add more products --
+
+select * from products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,79 @@
+// dependencies
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+require("console.table");
+var chalk = require("chalk")
+//establish connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_db",
+});
+//establish connection w/ sql db, in case of error will display error message
+connection.connect(function (error) {
+    if (error) {
+        console.log(chalk.red("error connecting", error))
+    }
+    displayProducts();
+})
+//displays all products from database function
+function displayProducts() {
+    console.log(chalk.blue("\n Welcome to bamazon! \n"));
+    connection.query("Select * from products", function (error, response) {
+        if (error) throw error
+        // console.log("sql response", response) - used as an example to differentiate log and table
+        // will display the products in table format 
+        console.log(chalk.black.bgYellow("Displaying stock for all available products \n"));
+        console.table(response);
+        userInput();
+    })
+
+
+}
+
+function userInput() {
+    inquirer.prompt([{
+                type: "input",
+                name: "itemId",
+                message: "What is the ID of the item you would like to purchase? [press 'Q' to quit]",
+                validate: function(input){
+                    console.log(input)
+                    if (input.toLowerCase() === 'q') {
+                        console.log("Thank you for shopping at Bamazon!")
+                        //Close Connection
+                        connection.end();
+                        //exits the app
+                        process.exit(0);
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "What quantity would you like to buy?",
+            }
+        ])
+
+        .then(function (inquirerResponse) {
+            //checking if product exists
+            connection.query('SELECT * FROM products WHERE item_id = ? ;', [inquirerResponse.itemId], function (error, response) {
+                if (error) throw error
+                // console.log("sql response", response) - used as an example to differentiate log and table
+                // will display the products in table format 
+                console.log("Checking Inventory");
+                if (inquirerResponse.quantity > response[0].stock_quantity) {
+                    console.log(chalk.white.bgRed.bold("Insufficient quantity. Your order cannot be placed."))
+                } else {
+                    connection.query("Update products set stock_quantity = stock_quantity - ? WHERE item_id = ?", [inquirerResponse.quantity, inquirerResponse.itemId], function (err, res) {
+                        if (err) throw err
+
+                        console.log(chalk.black.bgGreen.bold("Succesfully purchased " + inquirerResponse.quantity + " " + response[0].product_name + "(s).\n"));
+                    })
+
+                }
+                displayProducts();
+            })
+        })
+}
